1e5d252db09019d4aee93e31220dc1fd181d6467
Styp Canto <styp611@outlook.com>
2023-08-18 12:58:53 -0500

    Creando el reto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
+  purge:[],
+  darkMode: false,
   theme: {
     extend: {},
+  },
+  variants:{
+    opacity: ['responsive','hover','focus','disabled'],
+    backgroundColor: ['active','hover','focus','disabled'],
+    ringWidth:['active','focus'],
+    textColor:['disabled'], 
   },
   plugins: [],
 }
